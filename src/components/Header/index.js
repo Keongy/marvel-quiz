168d4ba158d456f96c87d99160e267b0dae437be
keongy <info.pichon@gmail.com>
2022-01-20 17:43:35 +0100

    Fix logo redirect

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
             <div className="banner-container">
-                <h1><a href="/marvel-quiz">Marvel Quiz</a></h1>
+                <h1><Link to='/marvel-quiz'>Marvel Quiz</Link></h1>
             </div>
         </header>
     )
