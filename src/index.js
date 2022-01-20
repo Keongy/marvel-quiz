0aecf70ec74653e6c9dad6a074b6597e38cb0a46
keongy <info.pichon@gmail.com>
2022-01-20 17:22:18 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import App from './components/App/index.js';
 import FirebaseContext from './components/Firebase/context.js';
 import Firebase from './components/Firebase/firebase.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <FirebaseContext.Provider value={new Firebase()}>
         <App />
       </FirebaseContext.Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
